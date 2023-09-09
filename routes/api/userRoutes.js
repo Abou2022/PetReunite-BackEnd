737ef01bdb7cb6aa8ea76f9c3a1ac4e3e407db07
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-08 18:04:16 -0700

    post and delete added to user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,9 +5,37 @@ const bcrypt = require("bcrypt");
 
 const router = require("express").Router();
 
+//Get All
 router.get("/", async (req, res) => {
   try {
     const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Create a post
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData) {
+      res.status(404).json({ message: "No user found with this data" });
+      return;
+    }
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
