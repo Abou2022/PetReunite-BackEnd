bf59c1cef2601a3107a5fd87e08dec58fae1c586
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-08 18:44:40 -0700

    routes update user added

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,7 +15,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-//Create a post
+//Create a user
 router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -25,6 +25,17 @@ router.post("/", async (req, res) => {
   }
 });
 
+//Update a user
+router.put("/:id", async (req, res) => {
+  try {
+    const userData = await User.update(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//Delete a User
 router.delete("/:id", async (req, res) => {
   try {
     const userData = await User.destroy({
