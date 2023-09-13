81b70784f01add4b0a5f9533c15151f3b91564da
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-12 18:06:25 -0700

    furryRoutes post updated date retreive fron insomina

--- routes/api/furryRoutes.js
+++ routes/api/furryRoutes.js
@@ -1,85 +1,70 @@
-// const express = require("express");
 const router = require("express").Router();
 const { Furry, User } = require("../../models");
 const bcrypt = require("bcrypt");
 
 // find all
-router.get("/", (req, res) => {
-  Furry.findAll({
-    include: [User],
-  })
-    .then((dbFurrys) => {
-      res.json(dbFurrys);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ msg: "an error occured", err });
+router.get("/", async (req, res) => {
+  try {
+    const dbFurrys = await Furry.findAll({
+      include: [User],
     });
+    res.json(dbFurrys);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "an error occurred", err });
+  }
 });
 
 // find one
-router.get("/:id", (req, res) => {
-  Furry.findByPk(req.params.id, {})
-    .then((dbFurrys) => {
-      res.json(dbFurrys);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ msg: "an error occured", err });
-    });
+router.get("/:id", async (req, res) => {
+  try {
+    const dbFurrys = await Furry.findByPk(req.params.id, {});
+    res.json(dbFurrys);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "an error occurred", err });
+  }
 });
 
 // create Furry
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const newFurry = {
-    ...req.body,
-    user_id: req.session.user.id,
-  };
-  Furry.create(newFurry)
-    .then((newFurry) => {
-      req.session.Furry = {
-        id: newFurry.id,
-        Furryname: newFurry.Furryname,
-      };
-      res.json(newFurry);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ msg: "an error occured", err });
-    });
+
+router.post("/", async (req, res) => {
+  try {
+    const userData = await Furry.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // update Pet
-router.put("/:id", (req, res) => {
-  Furry.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((updatedFurry) => {
-      res.json(updatedFurry);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ msg: "an error occured", err });
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedFurry = await Furry.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
     });
+    res.json(updatedFurry);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "an error occurred", err });
+  }
 });
 
 // delete a Pet
-router.delete("/:id", (req, res) => {
-  Furry.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((delFurry) => {
-      res.json(delFurry);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ msg: "an error occured", err });
+router.delete("/:id", async (req, res) => {
+  try {
+    const delFurry = await Furry.destroy({
+      where: {
+        id: req.params.id,
+      },
     });
+    res.json(delFurry);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "an error occurred", err });
+  }
 });
 
 module.exports = router;
