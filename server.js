3fd2853dfc35b0a0ecd9a0cbd0b45c1248030489
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-19 10:41:51 -0700

    server updated

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
-// const routes = require("./routes");
-const routes = require("./routes");
-
-// import sequelize connection
-
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+
+const furryRoutes = require("./routes/api/furryRoutes");
+const userRoutes = require("./routes/api/userRoutes");
+
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use("/api/furry", furryRoutes);
+app.use("/api/user", userRoutes);
 
-app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
