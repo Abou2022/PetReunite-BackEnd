a424f9133d8c4da4c20814150dd9058b0c05e8e4
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-26 04:17:43 -0700

    js folder and login script added

---
+++ js/login.js
@@ -0,0 +1,23 @@
+document.querySelector("#submit").addEventListener("click", (e) => {
+  e.preventDefault();
+  const userObj = {
+    username: document.querySelector("#loginUsername").value,
+    password: document.querySelector("#loginPassword").value,
+    email: document.querySelector("#loginEmail").value,
+  };
+  console.log(userObj);
+  fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify(userObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      console.log("logged in!");
+      location.href = "/dashboard";
+    } else {
+      alert("trumpet sound");
+    }
+  });
+});
