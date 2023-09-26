ba585041dc49190e91f89a82bd2f6b0b7058cbbb
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-26 04:29:29 -0700

    login script updated

--- js/login.js
+++ js/login.js
@@ -1,23 +1,25 @@
-document.querySelector("#submit").addEventListener("click", (e) => {
-  e.preventDefault();
-  const userObj = {
-    username: document.querySelector("#loginUsername").value,
-    password: document.querySelector("#loginPassword").value,
-    email: document.querySelector("#loginEmail").value,
-  };
-  console.log(userObj);
-  fetch("/api/users/login", {
-    method: "POST",
-    body: JSON.stringify(userObj),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    if (res.ok) {
-      console.log("logged in!");
-      location.href = "/dashboard";
-    } else {
-      alert("trumpet sound");
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models");
+
+// Define the login route
+router.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if a user with the provided email and password exists in your database
+    const user = await User.findOne({ where: { email, password } });
+
+    if (!user) {
+      return res.status(401).json({ error: "Login failed" });
     }
-  });
+
+    // Successful login, you can send user data as a response if needed
+    res.json(user);
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
+
+module.exports = router;
