2a6f078e3acce334d20aa76a3e46d44482a617ea
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-04 04:25:51 -0700

    loginRoute updated

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 
 const furryRoutes = require("./furryRoutes");
 const userRoutes = require("./userRoutes");
+const loginRoutes = require("./loginRoutes");
 
 router.use("/furry", furryRoutes);
 router.use("/user", userRoutes);
-router.use("/login", loginRoutes)
+router.use("/login", loginRoutes);
 
 module.exports = router;
