589070a93cda53fbcc936dbf425a09cbf59a8040
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-04 22:08:00 -0700

    signup route index update

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 
 const furryRoutes = require("./furryRoutes");
+const { default: signUpRoute } = require("./signUpRoute");
 const userRoutes = require("./userRoutes");
 // const loginRoutes = require("./loginRoutes");
 
 router.use("/furry", furryRoutes);
 router.use("/user", userRoutes);
+router.use("/signup", signUpRoute);
 // router.use("/login", loginRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,5 @@ router.use("/api", apiRoutes);
 router.use((req, res) => {
   res.send("<h1> Wrong route, you need to update your Route!</h1>");
 });
-// const userRoutes = require("./api/userRoutes");
-// router.use("/api/users", userRoutes);
-
-// const furryRoutes = require("./api/furryRoutes");
-// router.use("/api/furry", furryRoutes);
-
-// router.get("/sessions", (req, res) => {
-//   res.json(req.session);
-// });
 
 module.exports = router;
