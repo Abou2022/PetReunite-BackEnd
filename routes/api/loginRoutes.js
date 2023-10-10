48f358553c05a7e3d71f7eec5ed5c7a5b4163740
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-09 22:21:18 -0700

    updated login route

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -1,59 +1,51 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import db from "../../config/connection"; // Import your MySQL connection
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models");
 
-export const loginRoutes = {
-  path: "/api/login",
-  method: "post",
-  handler: async (req, res) => {
+const router = express.Router();
+
+// Login Route
+router.post("/", async (req, res) => {
+  try {
     const { email, password } = req.body;
 
-    try {
-      // Check if the user exists
-      const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [
-        email,
-      ]);
+    // Check if the user exists
+    const user = await User.findOne({ where: { email } });
 
-      if (rows.length === 0) {
-        return res.sendStatus(404); // User not found
-      }
-
-      const user = rows[0];
-
-      // Compare the hashed password
-      const passwordMatch = await bcrypt.compare(password, user.passwordHash);
-
-      if (!passwordMatch) {
-        return res.sendStatus(401); // Unauthorized (password incorrect)
-      }
-
-      // Create a JWT token
-      jwt.sign(
-        {
-          id: user.id,
-          email: user.email,
-          info: {
-            userFirstName: user.userFirstName,
-            userLastName: user.userLastName,
-          },
-          isVerified: user.isVerified,
-        },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "2d",
-        },
-        (err, token) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-          res.status(200).json({ token });
-        }
-      );
-    } catch (error) {
-      console.error("Error in login:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+    if (!user) {
+      return res.sendStatus(404); // User not found
     }
-  },
-};
 
-export default loginRoutes;
+    // Compare the hashed password
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+
+    if (!passwordMatch) {
+      return res.sendStatus(401); // Unauthorized (password incorrect)
+    }
+
+    // Create a JWT token
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        info: {
+          userFirstName: user.userFirstName,
+          userLastName: user.userLastName,
+        },
+        isVerified: user.isVerified,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2d",
+      }
+    );
+
+    res.status(200).json({ token });
+  } catch (err) {
+    console.error("Error in login:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
