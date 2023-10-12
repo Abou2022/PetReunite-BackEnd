216858461196fd01de18a88000b35d7c4bb55377
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-11 22:00:56 -0700

    server updated restore

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require("express");
+//new import
+// const mysql = require("mysql");
+// import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+
 // import express from "express";
-const mysql = require("mysql");
-import jwt from "jsonwebtoken";
+
+const express = require("express");
 const app = express();
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
@@ -16,7 +22,8 @@ const loginRoutes = require("./routes/api/loginRoutes");
 // app.use(routes);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //new
+app.use(cookieParser()); //new
 app.use("/api/furry", furryRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/signup", signUpRoute);
@@ -27,6 +34,19 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
 app.use(cors(corsOptions));
+
+//new connection
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   dialect: "mysql",
+
+//   password: "",
+//   database: "",
+// });
+
+// app.listen(8081, () => {
+//   console.log("Runing...");
+// });
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
