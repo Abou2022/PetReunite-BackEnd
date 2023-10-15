ec5f3d0c89fe5268a93663348dab74d9c719fff0
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-15 14:52:47 -0700

    loginRoutes updated with jwt token

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -20,6 +20,22 @@ export const loginRoutes = {
     if (!user) return res.sendStatus(401);
 
     const { _id: id, isVerified, passwordHash, info } = user;
+
+    const isCorrect = await bcrypt.compare(password, passwordHash);
+
+    if (isCorrect) {
+      jwt.sign(
+        { id, isVerified, email, info },
+        process.env.JWT_SECRET,
+        { expiresIn: "2d" },
+        (err, token) => {
+          if (err) {
+            res.status(500).json(err);
+          }
+          res.status(200).json({ token });
+        }
+      );
+    }
   },
 };
 // Login Route
