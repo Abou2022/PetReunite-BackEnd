d839866c420e3159b0109b6fc7782875b6d92425
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-15 14:40:13 -0700

    loginroute updated

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -5,47 +5,58 @@ const { User } = require("../../models");
 
 const router = express.Router();
 
-// Login Route
-router.post("/", async (req, res) => {
-  try {
+const { getDbConnection } = require("../../db");
+
+export const loginRoutes = {
+  path: "/api/loging",
+  method: "post",
+  handler: async (req, res) => {
     const { email, password } = req.body;
 
-    // Check if the user exists
-    const user = await User.findOne({ where: { email } });
+    const db = getDbConnection("petreunite_db");
+  },
+};
+// Login Route
+// router.post("/", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
 
-    if (!user) {
-      return res.sendStatus(404); // User not found
-    }
+//     // Check if the user exists
+//     const user = await User.findOne({ where: { email } });
 
-    // Compare the hashed password
-    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+//     if (!user) {
+//       return res.sendStatus(404); // User not found
+//     }
 
-    if (!passwordMatch) {
-      return res.sendStatus(401); // Unauthorized (password incorrect)
-    }
+//     // Compare the hashed password
+//     const passwordMatch = await bcrypt.compare(password, user.passwordHash);
 
-    // Create a JWT token
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        info: {
-          userFirstName: user.userFirstName,
-          userLastName: user.userLastName,
-        },
-        isVerified: user.isVerified,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "2d",
-      }
-    );
+//     if (!passwordMatch) {
+//       return res.sendStatus(401); // Unauthorized (password incorrect)
+//     }
 
-    res.status(200).json({ token });
-  } catch (err) {
-    console.error("Error in login:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+//     // Create a JWT token
+//     const token = jwt.sign(
+//       {
+//         id: user.id,
+//         email: user.email,
+//         info: {
+//           userFirstName: user.userFirstName,
+//           userLastName: user.userLastName,
+//         },
+//         isVerified: user.isVerified,
+//       },
+//       process.env.JWT_SECRET,
+//       {
+//         expiresIn: "2d",
+//       }
+//     );
+
+//     res.status(200).json({ token });
+//   } catch (err) {
+//     console.error("Error in login:", err);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 module.exports = router;
