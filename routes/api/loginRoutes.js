4b48dccaa44b6bd73b75aa38d86d53f18c5c524a
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-17 22:20:38 -0700

    loginRoute updated

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -10,6 +10,10 @@ router.post("/", async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res.sendStatus(400); // Bad request: Missing email or password
+    }
+
     // Check if the user exists
     const user = await User.findOne({ where: { email } });
 
@@ -18,27 +22,31 @@ router.post("/", async (req, res) => {
     }
 
     // Compare the hashed password
-    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+    if (user.passwordHash && typeof user.passwordHash === "string") {
+      const passwordMatch = await bcrypt.compare(password, user.passwordHash);
 
-    if (passwordMatch) {
-      // Create a JWT token and send a response
-      const token = jwt.sign(
-        {
-          id: user.id,
-          email: user.email,
-          info: {
-            userFirstName: user.userFirstName,
-            userLastName: user.userLastName,
+      if (passwordMatch) {
+        // Create a JWT token and send a response
+        const token = jwt.sign(
+          {
+            id: user.id,
+            email: user.email,
+            info: {
+              userFirstName: user.userFirstName,
+              userLastName: user.userLastName,
+            },
+            isVerified: user.isVerified,
           },
-          isVerified: user.isVerified,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "2d" }
-      );
+          process.env.JWT_SECRET,
+          { expiresIn: "2d" }
+        );
 
-      res.status(200).json({ token });
+        res.status(200).json({ token });
+      } else {
+        res.sendStatus(401); // Unauthorized (password incorrect)
+      }
     } else {
-      res.sendStatus(401); // Unauthorized (password incorrect)
+      res.sendStatus(500); // Internal Server Error: Password hash missing or invalid
     }
   } catch (err) {
     console.error("Error in login:", err);
