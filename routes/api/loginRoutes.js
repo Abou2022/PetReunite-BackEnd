a93364ac41db50ba4d07004fb90c5fe60ec1e126
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-18 22:31:34 -0700

    loginRoute error fixed

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -5,45 +5,52 @@ const { User } = require("../../models");
 
 const router = express.Router();
 
+// Login Route
 router.post("/", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Find the user by email in my database
+    // Check for missing email or password
+    if (!email) {
+      return res.status(400).json({ error: "Email is missing" });
+    }
+    if (!password) {
+      return res.status(400).json({ error: "Password is missing" });
+    }
+
+    // Check if the user exists
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(401).json({ error: "Invalid email or password" });
     }
 
     // Compare the hashed password
-    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
-    if (!passwordMatch) {
-      return res
-        .status(401)
-        .json({ error: "Unauthorized - Incorrect password" });
-    }
-
-    // Create a JWT token
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        info: {
-          userFirstName: user.userFirstName,
-          userLastName: user.userLastName,
+    if (passwordMatch) {
+      // Create a JWT token and send a response
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          info: {
+            userFirstName: user.userFirstName,
+            userLastName: user.userLastName,
+          },
+          isVerified: user.isVerified,
         },
-        isVerified: user.isVerified,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "2d" } // Token expiration time
-    );
+        process.env.JWT_SECRET,
+        { expiresIn: "2d" }
+      );
 
-    res.status(200).json({ token });
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid email or password" });
+    }
   } catch (err) {
     console.error("Error in login:", err);
-    res.status(500).json({ error: "Internal Server Error..." });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
